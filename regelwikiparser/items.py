5ac4f0053a901f1e1fde211067bb17595e38cf4c
David Pfahler <pfahler.david@gmail.com>
2016-12-18 16:39:17 +0100

    [FEATURE] better parsing

--- regelwikiparser/items.py
+++ regelwikiparser/items.py
@@ -5,16 +5,14 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
+class SpellClassItem(Item):
+    spellclass = Field()
+    spells = Field()
 
-class SpellsItem(scrapy.Item):
-    name = scrapy.Field()
-    spells = scrapy.Field()
-
-class SpellItem(scrapy.Item):
-    name = scrapy.Field()
-    properties = scrapy.Field()
-    
-class PropertyItem(scrapy.Item):
-	properties = scrapy.Field()
+class SpellItem(Item):
+    spellclass = Field()
+    name = Field()
+    link = Field()
+    properties = Field()
--- regelwikiparser/spiders/zauber_spider.py
+++ regelwikiparser/spiders/zauber_spider.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider, Request
+from regelwikiparser.items import SpellItem, SpellClassItem
 
-class Magic(scrapy.Spider):
+class Magic(Spider):
   name = "magic"
 
   def start_requests(self):
@@ -11,25 +12,46 @@ class Magic(scrapy.Spider):
     magic_tricks = "http://www.ulisses-regelwiki.de/index.php/Zauber_Zaubertricks.html"
     magic_classes  = {
         "Rituale": rituals,
-        "Zaubersprueche": spells,
+        #"Zaubersprueche": spells,
         "Zaubertricks": magic_tricks
       }
 
     for class_name, url in magic_classes.iteritems():
       print("start with " + class_name)
-      items[] = SpellsItem(name = class_name)
-      yield scrapy.Request(url=url, callback=self.parse)
+      item = SpellClassItem()
+      item['spellclass'] = class_name
+      item['spells'] = []
+      yield Request(
+        url=url,
+        callback=self.parse,
+        meta={'item': item}
+      )
+      yield item
+
 
   def parse(self, response):
 
+    spellclass_item = response.meta['item']
     for a in response.css('nav.mod_navigation a'):
       spell_name = a.css('a::attr(title)').extract_first()
+      print(spell_name)
       link = a.css('a::attr(href)').extract_first()
       if link is not None:
+        item = SpellItem()
+        item['name'] = spell_name
         spell_link = response.urljoin(link)
-        yield scrapy.Request(spell_link, callback=self.parse_spell)
+        item['link'] = spell_link
+        yield Request(
+          spell_link,
+          callback=self.parse_spell,
+          meta={'spell': item}
+        )
+        spellclass_item['spells'].append(item)
 
   def parse_spell(self, response):
+
+    spell_item = response.meta['spell']
+    spell_item['properties'] = {}
 
     properties = [
       "Probe",
@@ -44,15 +66,12 @@ class Magic(scrapy.Spider):
       "Steigerungsfaktor"
     ]
 
-    name_query = "//*/div/h1/text()"
-    selector = response.xpath(name_query)
-    name = selector.extract()
+    #name_query = "//*/div/h1/text()"
+    #selector = response.xpath(name_query)
+    #name = selector.extract()
 
-    item = PropertyItem()
-    item['properties'] = {}
     for p in properties:
       p_query = "//*/p/strong[text() = '" + p + ":']/following-sibling::text()[1]"
       selector = response.xpath(p_query)
-      item['properties'][p] = str(selector.extract()).lstrip()
-
-    items[response.meta['class']][response.meta['name']]['properties'] = item
+      spell_item['properties'][p] = str(selector.extract()).lstrip()
+    yield spell
