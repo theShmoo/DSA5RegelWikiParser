03342a790a437b31136bf0408af89842edb80113
David Pfahler <pfahler.david@gmail.com>
2017-03-02 12:31:23 +0100

    now parsing with regex.

---
+++ regelwikiparser/spiders/properties_parser.py
@@ -0,0 +1,153 @@
+import re
+import logging
+
+
+class PropertiesParser(object):
+
+    """docstring for PropertiesParser"""
+
+    def __init__(self, spell_info, spell_class):
+        super(PropertiesParser, self).__init__()
+        self.spell_info = spell_info
+        self.spell_class = spell_class
+
+    def filterPropertiesText(self, text, prop_rx):
+        # filter unnessary html format stuff
+        replace_operations = [
+            r"</?div( \w+\=\".+?\")?>",
+            r"</?span( \w+\=\".+?\")?>",
+            r"<p>\s*</p>",
+            r"<h1>(.*)</h1>"
+        ]
+        for op in replace_operations:
+            text = re.sub(op, "", text, 0, re.U)
+
+        # remove the strongs
+        str_rx = r"\s*<strong>\s*" + prop_rx + r":?\s*</strong>\s*:?\s*"
+        text = re.sub(str_rx, r"\1: ", text, 0, re.U)
+
+        # parse the Publikation
+        pub_rx = r"(<p>)?.*Publikation:\s?([\w\d ]+).*(</p>)?"
+        m = re.search(pub_rx, text, re.U)
+        if m is not None:
+            publication = m.group(2)
+        else:
+            publication = None
+
+        # filter out end keywords
+        end_keywords = [
+            r"Zaubererweiterungen",
+            r"Publikation"
+        ]
+        end_rx = r"("
+        for key in end_keywords[:-1]:
+            end_rx += key + r"|"
+        end_rx += end_keywords[-1] + r")(.|\n)*"
+        text = re.sub(end_rx, "", text, 0, re.U)
+
+        return (text, publication)
+
+    def createPropRegEx(self, properties):
+        prop_rx = r"("
+        for key in properties[:-1]:
+            prop_rx += key + r"|"
+        prop_rx += properties[-1] + r")\s*"
+        return prop_rx
+
+    def filterProp(self, prop):
+        prop_operations = [
+            r"((<p>)?\s*((</?p>)|(</?br>)))+"
+        ]
+        for rx in prop_operations:
+            prop = re.sub(rx, "", prop, 0, re.U)
+        return prop
+
+    def parseByText(self, selector, item):
+        props = self.spell_info["properties"]
+        spell_properties = {}
+
+        # create a or regex of all properties
+        prop_rx = self.createPropRegEx(props)
+
+        # get the text of the selector
+        text = selector.extract_first()
+        # text = unicode(text, "utf-8")
+        # use unicode fag everywhere! (re.U (Unicode) flag)
+
+        # filter unnessary html format stuff
+        (text, pub) = self.filterPropertiesText(text, prop_rx)
+        spell_properties["Publikation"] = pub
+
+        prop_rx += r": "
+
+        # find the existing properties
+        found_props = re.findall(prop_rx, text, re.U)
+
+        # split the text by the properties
+        found_descr = re.split(prop_rx, text)
+
+        first_found = -1
+        # iterate over the found descriptions
+        for idx, elem in enumerate(found_descr):
+            # if the description matches a property
+            if elem in found_props:
+                first_found = idx if first_found < 0 else first_found
+                # save the next description as its descritpion
+                spell_properties[elem] = self.filterProp(found_descr[idx + 1])
+
+        if self.spell_info['pre-text'] is 1:
+            if first_found > 0:
+                descr = self.filterProp(found_descr[first_found - 1])
+                spell_properties['Wirkung'] = descr
+            else:
+                logging.warning("Pre Text Error! " + item["name"])
+
+        item["properties"] = spell_properties
+
+    def parseProperties(self, selector, item):
+        """parse the properties of a spell and returns it as a dict"""
+
+        self.parseByText(selector, item)
+
+        default = self.spell_info["default-verbreitung"]
+        if default:
+            item["properties"]["Verbreitung"] = default
+
+        if self.spell_class == "Zaubertrick":
+            if "Anmerkung" in item["properties"]:
+                item["properties"]["Verbreitung"] = item[
+                    "properties"]["Anmerkung"]
+                item["properties"].pop("Anmerkung", None)
+
+        if "Verbreitung" not in item["properties"]:
+            item["properties"]["Verbreitung"] = "Allgemein"
+
+    def parseSpellExtensions(self, selector, item):
+        """parse the spell extensions of a spell and returns it as a dict"""
+
+        spell_extensions = {}
+
+        if self.spell_info['extension'] is 1:
+            extension_content_query = ".//em[contains(.,'#')]/" + \
+                "following-sibling::text()[1]"
+            extension_title_query = ".//em/text()[contains(.,'#')]"
+            title_selector = selector.xpath(extension_title_query)
+            content_selector = selector.xpath(extension_content_query)
+            titles = title_selector.extract()
+            contents = content_selector.extract()
+            if len(titles) >= 2 and len(contents) >= 2:
+                for i in range(0, 3):
+                    spell_extensions[i] = (titles[i][1:], contents[i])
+            else:
+                logging.warning("No Spell extensions found!")
+
+        item['spellextensions'] = spell_extensions
+
+    def parseName(self, selector, item):
+        """tries to parse the name of spell and returns it as a string"""
+        name_selector = selector.xpath(".//h1/text()")
+        name = name_selector.extract_first()
+        if name is None:
+            logging.warning("No name found!")
+        else:
+            item['name'] = name
--- regelwikiparser/spiders/zauber_spider.py
+++ regelwikiparser/spiders/zauber_spider.py
@@ -4,8 +4,9 @@ from scrapy.spiders import CrawlSpider
 from scrapy.selector import Selector
 from scrapy.http import Request
 from regelwikiparser.items import SpellItem
-import logging
+from properties_parser import PropertiesParser
 import json
+import logging
 
 
 class Magic(CrawlSpider):
@@ -21,6 +22,7 @@ class Magic(CrawlSpider):
     def start_requests(self):
         for spell_class, data in self.class_info.iteritems():
             if data['active'] is 1:
+                logging.info("Parsing links of " + spell_class)
                 request = Request(self.base_url + data['link'],
                                   callback=self.parseNavItems)
                 request.meta['type'] = spell_class
@@ -43,165 +45,6 @@ class Magic(CrawlSpider):
             request.meta['type'] = response.meta['type']
             yield request
 
-    def concatSelector(self, selector):
-        s = ""
-        node_contains_string = "./child::node()[count(*/self::strong)>0]"
-        for index, node in enumerate(selector):
-            if node.xpath(node_contains_string).extract():
-                logging.warning("strong detected!")
-                break
-            content = node.xpath("./text()").extract()
-            if isinstance(content, basestring):
-                s += content.lstrip()
-            elif len(content) > 0:
-                s += content[0].lstrip()
-        return s
-
-    def hasParagraphType(self, selector, item):
-        """uses the selector to check if the spell is easy to parse"""
-
-        q = ".//strong/ancestor-or-self::p[1]"
-        pars = selector.xpath(q)
-        for index, node in enumerate(pars):
-            num_x = node.xpath("count(./descendant::strong)")
-            num_strings = num_x.extract_first()
-            if num_strings is None or float(num_strings) != 1.0:
-                print("Bad Spell: " + item['name'])
-                print("Num Strongs: " + str(num_strings))
-                return 0
-        return 1
-
-    def parsePropertiesWithParagraphType(self, selector, item):
-        """parse all properties by paragraph type"""
-        props = self.class_info[item['spellclass']]['properties']
-        spell_properties = {}
-
-        for p in props:
-            q_strong = ".//strong[contains(.,\"" + p + ":\")]"
-            select_strong = selector.xpath(q_strong)
-            str_strong = select_strong.xpath("string(.)").extract_first()
-            if str_strong is not None:
-                q_all = "string(./ancestor-or-self::p[1])"
-                pars = select_strong.xpath(q_all)
-                str_all = pars.extract_first()
-                str_all = str_all.replace(str_strong, "")
-                spell_properties[p] = str_all
-            else:
-                logging.warning("Property " + p + " for " + item['name'] +
-                                " not found")
-                spell_properties[p] = ""
-
-        item['properties'] = spell_properties
-
-    def parseProperties(self, selector, item):
-        """parse the properties of a spell and returns it as a dict"""
-
-        properties = [
-            "Probe",
-            "Ritualdauer",
-            "Zauberdauer",
-            "AsP-Kosten",
-            "Reichweite",
-            "Wirkungsdauer",
-            "Zielkategorie",
-            "Merkmal",
-            "Talent",
-            "Verbreitung",
-            "Steigerungsfaktor",
-            "AP-Wert",
-            "Volumen",
-            "Voraussetzungen",
-            "Schutzkreis",
-            "Bannkreis",
-            "Tierarten"
-        ]
-
-        spell_properties = {}
-
-        s_class = item['spellclass']
-
-        # first check the type of the formatting
-        p_type = self.hasParagraphType(selector, item)
-
-        if p_type is 1:
-            self.parsePropertiesWithParagraphType(selector, item)
-        else:
-            if s_class is "Zaubertrick" or s_class is "Ahnenzeichen":
-
-                properties.append("Anmerkung")
-                # query for the "Wirkung"
-                # replace text() with node() to get the node
-                q = ".//text()[1]"
-                trick_response = selector.xpath(q)
-                e = trick_response.extract()
-                # the third one is always(?) the Wirkung
-                spell_properties["Wirkung"] = e[2]
-            else:
-                properties.append("Wirkung")
-
-            for p in properties:
-                p_query = ".//strong[contains(.,'" + p + ":')]" + \
-                    "/following::node()"
-                p_select = selector.xpath(p_query)
-                if p_select:
-                    s = self.concatSelector(p_select)
-                    if p == "Anmerkung":
-                        p = "Verbreitung"
-                    spell_properties[p] = s
-
-            if s_class is "Stabzauber" or \
-                    s_class is "Bann und Schutzkreis":
-                spell_properties["Verbreitung"] = "Gildenmagier"
-
-            if s_class is "Fluch" or s_class is "Vertrautentrick":
-                spell_properties["Verbreitung"] = "Hexen"
-
-            if s_class is "Dolchritual" or s_class is "Herrschaftsritual":
-                spell_properties["Verbreitung"] = "Druiden"
-
-            if s_class is "Elfenlied" or \
-                    s_class is "Verzerrtes Elfenlied":
-                spell_properties["Verbreitung"] = "Elfen"
-            item['properties'] = spell_properties
-
-    def parseSpellExtensions(self, selector, item):
-        """parse the spell extensions of a spell and returns it as a dict"""
-
-        spell_extensions = {}
-
-        if self.class_info[item['spellclass']]['extension'] is 1:
-            extension_content_query = ".//em[contains(.,'#')]/" + \
-                "following-sibling::text()[1]"
-            extension_title_query = ".//em/text()[contains(.,'#')]"
-            title_selector = selector.xpath(extension_title_query)
-            content_selector = selector.xpath(extension_content_query)
-            titles = title_selector.extract()
-            contents = content_selector.extract()
-            if len(titles) >= 2 and len(contents) >= 2:
-                for i in range(0, 3):
-                    spell_extensions[i] = (titles[i][1:], contents[i])
-            else:
-                logging.warning("No Spell extensions found!")
-
-        item['spellextensions'] = spell_extensions
-
-    def parseName(self, selector, item):
-        """tries to parse the name of spell and returns it as a string"""
-        name_selector = selector.xpath(".//h1/text()")
-        name = name_selector.extract_first()
-        if name is None:
-            logging.warning("No name found!")
-        else:
-            item['name'] = name
-
-    def parseSpellClass(self, spell_class, item):
-        """returns the already known spell class. (useless)"""
-
-        if spell_class:
-            item['spellclass'] = spell_class
-        else:
-            logging.warning("Spell Class not found!")
-
     def parseSpell(self, response):
 
         item = SpellItem()
@@ -209,20 +52,26 @@ class Magic(CrawlSpider):
         # save the link
         item['link'] = response.url
 
+        spell_class = response.meta['type']
+        if spell_class:
+            item['spellclass'] = spell_class
+        else:
+            logging.warning("Spell Class not found!")
+
         # get the important divs (can be 2)
         importantDivs = "//div[@id='main']//div[contains(@class,'ce_text')]"
         i_select = response.xpath(importantDivs)
 
-        # extract name
-        self.parseName(i_select, item)
+        # create the properties parser
+        pp = PropertiesParser(self.class_info[spell_class], spell_class)
 
-        # parse for spell class
-        self.parseSpellClass(response.meta['type'], item)
+        # extract the name
+        pp.parseName(i_select, item)
 
-        # parse for the properties
-        self.parseProperties(i_select, item)
+        # extract the properties
+        pp.parseProperties(i_select, item)
 
-        # parse for the spell extensions
-        self.parseSpellExtensions(i_select, item)
+        # extract the spell extensions
+        pp.parseSpellExtensions(i_select, item)
 
         return item
